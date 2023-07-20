6a61f86bfd49e7641eaf4ffdd00715b980844567
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-20 18:17:23 +0200

    Make UpperCase - completed

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,11 @@
+// Write a function which converts the input string to uppercase.
+
+//Solution
+function makeUpperCase(str) {
+  let upperCase = str.toUpperCase();
+
+  console.log(upperCase);
+  return upperCase;
+}
+
+makeUpperCase("hello");
