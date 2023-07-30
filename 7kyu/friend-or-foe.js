b410796dcb8411dd05e57d126cc794bc9f5f7a3b
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-30 03:44:56 +0200

    Friend or Foe? - completed

--- 7kyu/friend-or-foe.js
+++ 7kyu/friend-or-foe.js
@@ -12,15 +12,7 @@
 //Solution
 
 function friend(friends) {
-  let result = [];
-  for (let i = 0; i < friends.length; i++) {
-    if (friends[i].length === 4) {
-      result.push(friends[i]);
-    }
-  }
-
-  console.log(result);
-  return result;
+  return friends.filter((friend) => friend.length === 4);
 }
 friend(["Ryan", "Kieran", "Mark"]); // ["Ryan", "Mark"]
 friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]); // ["Ryan"]
